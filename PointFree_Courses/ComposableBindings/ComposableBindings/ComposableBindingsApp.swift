f6249383b8797923bcc30f64b7be33aecfe476bc
Julia Vorotchenko <juliavorotchenko@gmail.com>
2023-10-17 18:04:44 +0300

    Make workspace for multiprojects

---
+++ PointFree_Courses/ComposableBindings/ComposableBindings/ComposableBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableBindingsApp.swift
+//  ComposableBindings
+//
+//  Created by Юлія Воротченко on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ComposableBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
